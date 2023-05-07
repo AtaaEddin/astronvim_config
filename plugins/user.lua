567b80dcba2a1264dd8f2157f4bb46d47111db9a
mouhamad ataa al daghstani <ataa_co@hotmail.com>
2023-05-07 18:00:55 +0300

    Add user file & adjust mapping

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,17 @@
+-- TODO test
+-- HACK test
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function() require("todo-comments").setup() end,
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    },
+  },
 }
